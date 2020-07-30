2a118791512c785fabc67b8afe58bbf29cd139ae
aafutin <alex@afut.in>
2020-07-30 21:49:04 +0300

    add solution for 01-intro 01-sum

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,12 @@
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
 function sum(a, b) {
-  /* ваш код */
+  if (!isNumeric(a) || !isNumeric(b)) {
+    throw new TypeError('Допускаются только числа', 'sum.js', 7);
+  }
+  return a + b;
 }
 
 module.exports = sum;

5f4349d4e5ea9e2cb97ad332a82dd60b9faf198c
aafutin <alex@afut.in>
2020-08-09 22:08:48 +0300

    add solution for 05-http-server-streams 01-file-server-get

--- 05-http-server-streams/01-file-server-get/server.js
+++ 05-http-server-streams/01-file-server-get/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -11,7 +12,16 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'GET':
-
+      if (pathname.indexOf('/') !== -1) {
+        res.statusCode = 400;
+        return res.end();
+      }
+      const fileReadStream = fs.createReadStream(filepath);
+      fileReadStream.pipe(res);
+      fileReadStream.on('error', () => {
+        res.statusCode = 404;
+        res.end();
+      });
       break;
 
     default:

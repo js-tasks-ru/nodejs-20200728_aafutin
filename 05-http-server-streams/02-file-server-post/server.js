a2f730fd23b8f17fb6474ef62140fb5106615782
aafutin <alex@afut.in>
2020-08-11 01:16:11 +0300

    add solution for 05-http-server-streams 02-file-server-post

--- 05-http-server-streams/02-file-server-post/server.js
+++ 05-http-server-streams/02-file-server-post/server.js
@@ -1,16 +1,38 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
-
+const fs = require('fs-extra');
 const server = new http.Server();
+const {pipeline} = require('stream');
+const LimitSizeStream = require('./LimitSizeStream');
 
-server.on('request', (req, res) => {
+server.on('request', async (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
 
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'POST':
+      if (pathname.indexOf('/') !== -1) {
+        res.statusCode = 400;
+        return res.end();
+      } else if (await fs.exists(filepath)) {
+        res.statusCode = 409;
+        return res.end();
+      }
+
+      const limitStream = new LimitSizeStream({limit: 1048576});
+      const newFileStream = fs.createWriteStream(filepath);
+
+      pipeline(req, limitStream, newFileStream, async (err) => {
+        if (err) {
+          res.statusCode = err.code === 'LIMIT_EXCEEDED' ? 413 : 500;
+          await fs.remove(filepath);
+        } else {
+          res.statusCode = 201;
+        }
+        res.end();
+      });
 
       break;
 

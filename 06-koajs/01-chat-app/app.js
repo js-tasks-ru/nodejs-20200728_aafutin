8869bf67f581aca342456b7686f7a79842e52014
aafutin <alex@afut.in>
2020-09-12 22:08:45 +0300

    add solution for 06-koajs 01-chat-app

--- 06-koajs/01-chat-app/app.js
+++ 06-koajs/01-chat-app/app.js
@@ -8,10 +8,32 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+let subscribers = {};
+
 router.get('/subscribe', async (ctx, next) => {
+  const id = Math.random();
+  subscribers[id] = {};
+
+  ctx.req.on('close', function() {
+    delete subscribers[id];
+  });
+
+  return await new Promise((resolve) => {
+    const waitMessage = setInterval(() => {
+      if (subscribers[id].message) {
+        ctx.body = subscribers[id].message;
+        clearInterval(waitMessage);
+        resolve();
+      }
+    }, 10);
+  });
 });
 
 router.post('/publish', async (ctx, next) => {
+  for (const id in subscribers) {
+    subscribers[id].message = ctx.request.body.message;
+  }
+  ctx.body = '';
 });
 
 app.use(router.routes());

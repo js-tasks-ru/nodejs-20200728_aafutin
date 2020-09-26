40658b389551f951bffc84cae3b1e2cd550aded4
aafutin <alex@afut.in>
2020-09-26 19:59:00 +0300

    add solution for 07-mongodb-mongoose 02-rest-api

--- 07-mongodb-mongoose/02-rest-api/controllers/categories.js
+++ 07-mongodb-mongoose/02-rest-api/controllers/categories.js
@@ -1,3 +1,22 @@
+const Category = require('../models/Category');
+
+function mapSubcategory(subcategory) {
+  return {
+    id: subcategory.id,
+    title: subcategory.title,
+  };
+}
+
+function mapCategory(category) {
+  return {
+    id: category.id,
+    title: category.title,
+    subcategories: category.subcategories.map(mapSubcategory),
+  };
+}
+
 module.exports.categoryList = async function categoryList(ctx, next) {
-  ctx.body = {categories: []};
+  const categories = await Category.find();
+  ctx.body = {categories: categories.map(mapCategory)};
+  return next();
 };

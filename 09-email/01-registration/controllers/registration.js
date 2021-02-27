a5f4a68969469ab7194617c37843e9a1ea0fba4e
aafutin <alex@afut.in>
2021-02-28 00:11:13 +0300

    add solution for 09-email 02-order

--- 09-email/01-registration/controllers/registration.js
+++ 09-email/01-registration/controllers/registration.js
@@ -6,24 +6,9 @@ module.exports.register = async (ctx, next) => {
   const verificationToken = uuid();
   const { email, displayName, password } = ctx.request.body;
 
-  const userData = {
-    email,
-    displayName,
-    password,
-    verificationToken,
-  };
-  const user = new User(userData);
-  await user.setPassword(userData.password);
-  try {
-    await user.save();
-  } catch (err) {
-    ctx.status = 400;
-    ctx.body = { errors: {} };
-    for (const [field, error] of Object.entries(err.errors)) {
-      ctx.body.errors[field] = error.message;
-    }
-    return;
-  }
+  const user = new User({ email, displayName, verificationToken });
+  await user.setPassword(password);
+  await user.save();
 
   await sendMail({
     to: email,
@@ -33,7 +18,6 @@ module.exports.register = async (ctx, next) => {
   });
 
   ctx.body = { status: 'ok' };
-  return next();
 };
 
 module.exports.confirm = async (ctx, next) => {
@@ -45,5 +29,4 @@ module.exports.confirm = async (ctx, next) => {
   user.verificationToken = undefined;
   await user.save();
   ctx.body = { token: await ctx.login(user) };
-  return next();
 };
